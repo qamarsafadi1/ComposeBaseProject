ccac2ea98856eb0bdc9aab59828f4dd535a50479
Qamar Safadi <qamar.elsafadi98@gmail.com>
2023-02-21 13:04:09 +0200

    feat: change language

--- app/src/main/java/com/selsela/composebaseproject/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/selsela/composebaseproject/ui/screens/home/HomeScreen.kt
@@ -10,11 +10,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.selsela.composebaseproject.BaseApp.Companion.LocalData
+import com.selsela.composebaseproject.R
+import com.selsela.composebaseproject.data.local.PreferenceHelper.appLocale
 import com.selsela.composebaseproject.data.local.PreferenceHelper.configurations
 import com.selsela.composebaseproject.ui.core.components.Button
+import com.selsela.composebaseproject.util.LocalUtils.setLocale
 import com.selsela.composebaseproject.util.log
 
 @Composable
@@ -27,19 +32,29 @@ fun HomeScreen(
         Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-
+        val context = LocalContext.current
         Column(
             verticalArrangement = Arrangement.Center
         ) {
             Button(
                 onClick = onDataClick,
-                title = "Data"
+                title = stringResource(R.string.data)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {
+                    if (LocalData.appLocale == "ar")
+                        context.setLocale("en")
+                    else context.setLocale("ar")
+
+                },
+                title = stringResource(R.string.change_langauge)
             )
             if (viewModel.userLoggedIn.value.not()) {
                 Spacer(modifier = Modifier.height(10.dp))
                 Button(
                     onClick = onAuthClick,
-                    title = "Auth"
+                    title = stringResource(R.string.auth)
                 )
             }
         }
--- app/src/main/java/com/selsela/composebaseproject/util/LocalUtils.kt
+++ app/src/main/java/com/selsela/composebaseproject/util/LocalUtils.kt
@@ -1,11 +1,15 @@
 package com.selsela.composebaseproject.util
 
 import android.content.Context
+import android.icu.util.LocaleData
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
+import com.selsela.composebaseproject.BaseApp.Companion.LocalData
+import com.selsela.composebaseproject.data.local.PreferenceHelper.appLocale
 
 object LocalUtils {
     fun Context.setLocale(lang: String) {
+        LocalData.appLocale = lang
         val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags(lang)
         AppCompatDelegate.setApplicationLocales(appLocale)
     }
